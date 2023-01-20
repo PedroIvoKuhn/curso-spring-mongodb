68aab6403556aa6c7198dd0613d3310da7e4634a
Pedro Ivo <kuhnpedroivo@gmail.com>
2023-01-20 15:40:12 -0300

    Obtendo um usuario por id

---
+++ src/main/java/com/cursojava/workshopmongo/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,8 @@
+package com.cursojava.workshopmongo.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException{
+
+    public ObjectNotFoundException(String arg0) {
+        super(arg0);
+    }
+}
